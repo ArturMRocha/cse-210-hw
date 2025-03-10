75301eb44fdab97b53e11148130e537c74175bee
ArturMRocha <Nemezix9@outlook.com>
2025-03-10 19:29:15 -0300

    finished exerc 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Gera um número aleatório de 1 a 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int attempts = 0;
+
+            Console.WriteLine("Bem-vindo ao jogo 'Adivinhe o Meu Número'!");
+
+            // Loop para verificar se o usuário acertou o número mágico
+            while (guess != magicNumber)
+            {
+                Console.Write("Qual é o seu palpite? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Mais alto");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Mais baixo");
+                }
+                else
+                {
+                    Console.WriteLine($"Você acertou! Foram necessárias {attempts} tentativas.");
+                }
+            }
+
+            // Pergunta se o usuário quer jogar novamente
+            Console.Write("Você quer jogar novamente (sim/nao)? ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Obrigado por jogar! Até a próxima.");
     }
 }
+
+    }

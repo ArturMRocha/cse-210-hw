4c2d80587b70de903de7d7650e2c9729ec88affc
ArturMRocha <Nemezix9@outlook.com>
2025-03-19 16:52:50 -0300

    finished fraction week03

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,105 @@
 using System;
 
+public class Fraction
+{
+    // Private attributes for encapsulation
+    private int _numerator;
+    private int _denominator;
+
+    // Constructor with no parameters (defaults to 1/1)
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    // Constructor with one parameter (defaults denominator to 1)
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    // Constructor with two parameters
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        if (denominator != 0)
+        {
+            _denominator = denominator;
+        }
+        else
+        {
+            throw new ArgumentException("Denominator cannot be zero");
+        }
+    }
+
+    // Getter for numerator
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    // Setter for numerator
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    // Getter for denominator
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    // Setter for denominator
+    public void SetDenominator(int denominator)
+    {
+        if (denominator != 0)
+        {
+            _denominator = denominator;
+        }
+        else
+        {
+            throw new ArgumentException("Denominator cannot be zero");
+        }
+    }
+
+    // Method to get fraction as a string (e.g., "3/4")
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    // Method to get fraction as a decimal value (e.g., 0.75)
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Test first constructor (1/1)
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        // Test second constructor (5/1)
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        // Test third constructor (3/4)
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        // Test getters and setters
+        fraction3.SetNumerator(1);
+        fraction3.SetDenominator(3);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
